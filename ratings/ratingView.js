ae241ee51d7136b4b13bc1173e9dea30cd985b5b
Shiv19 <mailshiva19@gmail.com>
2017-11-07 18:53:05 +0530

    code optimization

--- ratings/ratingView.js
+++ ratings/ratingView.js
@@ -1,52 +1,34 @@
-const observableModule = require("data/observable");
-const ObservableArray = require("data/observable-array").ObservableArray;
+exports.ratingLabelLoaded = function(args) {
+    let ratingLabel = args.object;
+    var textContent = "";
+    setTimeout(() => {
+        var numberOfStars = args.object.finalRating;
+        const flooredVal = Math.floor(numberOfStars);
 
-exports.loaded = function (args) {
-    const component = args.object;
-    let numberOfStars = component.value;
-    const totalStars = component.max || 5;
-    const color = component.color || 'black';
-    const flooredVal = Math.floor(numberOfStars);
-    const ratingsBar = component.getViewById("ratingsBar");
-
-    let halfStars = false;
-
-    if (Math.ceil(numberOfStars) !== Math.floor(numberOfStars)) {
-        if (Math.round(numberOfStars) !== Math.ceil(numberOfStars)) {
-            halfStars = true;
-            numberOfStars = flooredVal;
-        } else {
-            numberOfStars = flooredVal + 1;
+        let halfStars = false;
+        let totalStars = 5;
+        if (Math.ceil(numberOfStars) !== Math.floor(numberOfStars)) {
+            if (Math.round(numberOfStars) !== Math.ceil(numberOfStars)) {
+                halfStars = true;
+                numberOfStars = flooredVal;
+            } else {
+                numberOfStars = flooredVal + 1;
+            }
         }
-    }
 
-    const viewModel = observableModule.fromObject({
-        items: new ObservableArray([]),
-        color: color
-    });
-
-    for (let i = 0; i < numberOfStars; i++) {
-        viewModel.items.push({
-            "type": "full", // currently useless, you can use it if you wish
-            "icon": "★" // set fontAwesome icons here
-        })
-    }
-    if (halfStars) {
-        viewModel.items.push({
-            "type": "half", // currently useless, you can use it if you wish
-            "icon": "✫" // set fontAwesome icons here
-        })
-        numberOfStars = numberOfStars + 1;
-    }
-    if ((totalStars - numberOfStars) > 0 ) {
-        let extra = totalStars - numberOfStars;
-        for (let i = 0; i < extra; i++) {
-            viewModel.items.push({
-                "type": "extra", // currently useless, you can use it if you wish
-                "icon": "☆" // set fontAwesome icons here
-            })
+        for (let i = 0; i < numberOfStars; i++) {
+            textContent += "★";
         }
-    }
-
-    ratingsBar.bindingContext = viewModel;
-}
+        if (halfStars) {
+            numberOfStars = numberOfStars + 1;
+            textContent += "✫";
+        }
+        if (totalStars - numberOfStars > 0) {
+            let extra = totalStars - numberOfStars;
+            for (let i = 0; i < extra; i++) {
+                textContent += "☆";
+            }
+        }
+        ratingLabel.text = textContent;
+    }, 50);
+};
